dd0debd3f497f9fcb387dfaa2e75adad4abda171
MarkOS2023 <markexplorer33@mail.ru>
2023-01-20 21:18:52 +0300

    анимация проба+ rec+ readme

---
+++ anim_for_game.py
@@ -0,0 +1,61 @@
+import pygame
+import os
+import sys
+
+def load_image(name):
+    fullname = os.path.join(name)
+    if not os.path.isfile(fullname):
+        print(f'Файл с изображением {fullname} не найден')
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+all_sprites = pygame.sprite.Group()
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+dragon = AnimatedSprite(load_image("131.jpg"), 8, 1, 250, 250)
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Space defender')
+    size = width, height = 600, 600
+    screen = pygame.display.set_mode(size)
+    fps = 50
+    clock = pygame.time.Clock()
+    screen_rect = (0, 0, width, height)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        #fon = pygame.transform.scale(load_image('Batl_fon2.png'), (width, height))
+        #screen.blit(fon, (0, 0))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        pygame.display.flip()
+        clock.tick(fps)
+        pygame.display.update()
+    pygame.quit()
