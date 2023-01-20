009317c60802af898fb108baec6d3659c68d374f
FalseKnigh123 <atruppikov@gmail.com>
2023-01-21 01:20:15 +0300

    Почти финальная версия

--- main.py
+++ main.py
@@ -27,17 +27,17 @@ bullet_group = pygame.sprite.Group()
 horizontal_border = pygame.sprite.Group()
 vertical_border = pygame.sprite.Group()
 ship_image = load_image("Ship (1).png")
-fon_dif = load_image("start.jpg")
+fon_dif = load_image("fon.png")
 metior_image = load_image("metior.png")
 metior_23_image = load_image("mid_met-tran.png")
 shooting_sound = pygame.mixer.Sound("data/sound/shoot.wav")
 explo_sound = pygame.mixer.Sound("data/sound/invaderkilled.wav")
 shooting_sound.set_volume(0.01)
 explo_sound.set_volume(0.01)
-game_over_sound = pygame.mixer.Sound("data/sound/main_sound.mp3")
-#game_over_sound = pygame.mixer.Sound("explosion.wav")
+game_over_sound = pygame.mixer.Sound("data/sound/1.mp3")
 bul_im = load_image("bul.png")
 help_im = load_image("help.png")
+cristal_im = load_image("Big_cristal.png")
 GRAVITY = 0.1
 scor = 0
 met_images = list()
@@ -60,9 +60,49 @@ def start_screen():
         best_scor = int(f.read())
     intro_text = ["Space defender", "", "", "",
                   f" Лучший результат: {best_scor}", "", "",
-                  "Назмите конпку 'spase' чтобы начать игры", ]
+                  "Назмите конпку 'spase' чтобы начать игры",
+                  "Назмите конпку на английскую кнопку 'H' чтобы",
+                  "узнать правила игры"]
     global width, height
     fon = pygame.transform.scale(load_image('start.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 100
+    pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
+    bullet_group = pygame.sprite.Group()
+    horizontal_border = pygame.sprite.Group()
+    vertical_border = pygame.sprite.Group()
+    enemy = pygame.sprite.Group()
+    for line in intro_text:
+        string_rendered = font.render(line, True, (255, 239, 213))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                return
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_h:
+                help_screen()
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+def help_screen():
+    global best_scor, help_grop, player_group, bullet_group, horizontal_border, \
+        all_sprites, vertical_border, enemy, width, height
+    intro_text = ["Правила игры:", "Нажимате кнопки wasd или стрелочки,",
+                  "чтобы управлять судном.",
+                  "Чтобы стрелять зажимате клавишу 'space.'", "", "Старайтесь уклонатся от метеоритов",
+                  "и собирать кристалики", "нажмите space чтобы продолжить"]
+    fon = pygame.transform.scale(fon_dif, (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 100
@@ -94,8 +134,8 @@ def start_screen():
 def dificult_screen():
     intro_text = ["Выберете сложность", "Нажмите на клавиатуре цифру обозночающую сложность",
                   "1.Легко Мало метеоритов", "2. Средняя сложность Метеоритов больше",
-                  "3.Сложно  Метеоритов еще больше колличестов", "хп увеличивается с размером",
-                  "4.impossible Метеоритов огромной колличесто,", "даже у самых мальньких метеоритов 2 хп"]
+                  "3.Сложно  Метеоритов еще больше колличестов",
+                  "4.impossible Метеоритов огромной колличесто,"]
     global width, height, lvl, fon_flag
     fon = pygame.transform.scale(fon_dif, (width, height))
     screen.blit(fon, (0, 0))
@@ -118,18 +158,16 @@ def dificult_screen():
                 fon_flag = 1
                 return
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_2:
-                lvl = 20
+                lvl = 15
                 fon_flag = 2
                 return
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_3:
-                lvl = 30
+                lvl = 25
                 fon_flag = 3
-                # To Do хр
                 return
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_4:
-                lvl = 40
+                lvl = 35
                 fon_flag = 4
-                # To Do хр
                 return
         pygame.display.flip()
         clock.tick(fps)
@@ -140,7 +178,7 @@ def final_screen():
     global width, height, running, scor
     intro_text = ["Defead", "", "", "",
                   f"Результат: {scor}", "", "",
-                  "Нажмите на кнопу SPASE чтобы вернуктся ", ""
+                  "Нажмите нaа кнопу SPASE чтобы вернуктся ", ""
                                                              "на стартовый экран"]
     fon = pygame.transform.scale(load_image('final_fon.png'), (width, height))
     screen.blit(fon, (0, 0))
@@ -160,13 +198,43 @@ def final_screen():
                 if best_scor < scor:
                     with open("schor", "w") as f:
                         f.write(str(scor))
-                terminate()
+                Control_screen()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                Control_screen()
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 running = False
                 if best_scor < scor:
                     with open("schor", "w") as f:
                         f.write(str(scor))
                 scor = 0
+                return
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+def Control_screen():
+    global running
+    intro_text = ["Вы точно хотите выйти?", "Нажмите escape, чтобы выйти",
+                  'Или нажмите spase чтобы продолжить']
+    fon = pygame.transform.scale(load_image('final_fon.png'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 100
+    for line in intro_text:
+        string_rendered = font.render(line, True, (255, 239, 213))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                terminate()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 return
         pygame.display.flip()
         clock.tick(fps)
@@ -230,7 +298,6 @@ class Buff(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(help_im, (25, 25))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        shooting_sound.play()
 
     def update(self):
         global scor, buff_flag
@@ -240,7 +307,40 @@ class Buff(pygame.sprite.Sprite):
         if pygame.sprite.spritecollide(self, player_group, False):
             self.kill()
             buff_flag = True
-            scor += 2
+            if fon_flag == 1:
+                scor += 1
+            if fon_flag == 2:
+                scor += 2
+            if fon_flag == 3:
+                scor += 4
+            if fon_flag == 4:
+                scor += 8
+
+
+class Buff_2(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Buff_2, self).__init__(all_sprites)
+        self.add(help_grop)
+        self.image = pygame.transform.scale(cristal_im, (25, 25))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self):
+        global scor, buff_flag
+        self.rect.y += 3
+        if self.rect.top < 0:
+            self.kill()
+        if pygame.sprite.spritecollide(self, player_group, False):
+            self.kill()
+            buff_flag = True
+            if fon_flag == 1:
+                scor += 5
+            if fon_flag == 2:
+                scor += 10
+            if fon_flag == 3:
+                scor += 20
+            if fon_flag == 4:
+                scor += 40
 
 
 class Bulet(pygame.sprite.Sprite):
@@ -263,7 +363,16 @@ class Bulet(pygame.sprite.Sprite):
             create_particles((self.rect.x, self.rect.y))
             if random.randrange(1, 10) == 1:
                 Buff(self.rect.x, self.rect.y)
-            scor += lvl / 5
+            if random.randrange(1, 50) == 1:
+                Buff_2(self.rect.x, self.rect.y)
+            if fon_flag == 1:
+                scor += 2
+            if fon_flag == 2:
+                scor += 4
+            if fon_flag == 3:
+                scor += 8
+            if fon_flag == 4:
+                scor += 16
 
 
 class Border(pygame.sprite.Sprite):
@@ -295,7 +404,7 @@ class Player(pygame.sprite.Sprite):
         global player_group, all_sprites, bullet_group, horizontal_border, vertical_border, enemy
         keys = pygame.key.get_pressed()
         time_now = pygame.time.get_ticks()
-        cooldown = 350
+        cooldown = 200
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.rect.x -= VEL
             if self.rect.x < 0:
@@ -347,7 +456,9 @@ if __name__ == '__main__':
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    Control_screen()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    Control_screen()
             if len(enemy) < lvl:
                 Metior(random.randrange(50, 80), random.randrange(0, 550), 20)
             if fon_flag == 1:
